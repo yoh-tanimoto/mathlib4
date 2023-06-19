5469320ac8661b633f7aacae631c9674cae5a1d7
Yury G. Kudryashov <urkud@urkud.name>
2023-06-18 20:38:14 -0500

    Snapshto

--- Mathlib/MeasureTheory/Group/MinimalErgodic.lean
+++ Mathlib/MeasureTheory/Group/MinimalErgodic.lean
@@ -12,31 +12,18 @@ import Mathlib.MeasureTheory.Integral.Average
 
 open Function Set Filter Topology MeasureTheory
 
+@[to_additive]
+theorem Filter.EventuallyEq.of_mulIndicator_const {α β : Type _} [One β] {l : Filter α} {c : β}
+    (hc : c ≠ 1) {s t : Set α} (h : s.mulIndicator (fun _ ↦ c) =ᶠ[l] t.mulIndicator fun _ ↦ c) :
+    s =ᶠ[l] t := by
+  have : ∀ s : Set α, s.mulIndicator (fun _ ↦ c) ⁻¹' {c} = s := fun s ↦ by
+    rw [mulIndicator_preimage_of_not_mem, preimage_const_of_mem (mem_singleton _), univ_inter]
+    exact hc.symm
+  simpa only [this] using h.preimage {c}
+
 variable {G E : Type _}
   [Group G] [TopologicalSpace G] [TopologicalGroup G] [CompactSpace G] [NormalSpace G]
   [MeasurableSpace G] [BorelSpace G]
   {μ : MeasureTheory.Measure G} [μ.WeaklyRegular] [μ.IsHaarMeasure]
   [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E]
 
-theorem L1.eq_average_of_dense_eq_comp_const_mul {f : G →₁[μ] E} {s : Set G} (hd : Dense s)
-    (hsf : ∀ a ∈ s, f =ᵐ[μ] (f <| a * ·)) :
-    f =ᵐ[μ] const G (⨍ x, f x ∂μ) := by
-  
-
-theorem Integrable.ae_eq_average_of_dense_ae_eq_comp_const_mul {f : G → E}
-    (hf : Integrable f μ) {s : Set G} (hd : Dense s) (hsf : ∀ a ∈ s, f =ᵐ[μ] (f <| a * ·)) :
-    f =ᵐ[μ] const G (⨍ x, f x ∂μ) := by
-  -- have := hf.exists_boundedContinuous_integral_sub_le
-  
-  -- wlog hf' : ∃ f' : G →₁[μ] E, f = f' generalizing f
-  -- · calc
-  --     f =ᵐ[μ] hf.toL1 f := hf.coeFn_toL1.symm
-  --     _ =ᵐ[μ] const G (⨍ x, hf.toL1 f x ∂μ) := by
-  --       refine this (AEEqFun.integrable_iff_mem_L1.2 (hf.toL1 _).2) (fun a ha ↦ ?_) ⟨_, rfl⟩
-  --       calc
-  --         hf.toL1 f =ᵐ[μ] f := hf.coeFn_toL1
-  --         _ =ᵐ[μ] (f <| a * ·) := hsf a ha
-  --         _ =ᵐ[μ] (hf.toL1 f <| a * ·) :=
-  --           (measurePreserving_mul_left μ a).quasiMeasurePreserving.ae hf.coeFn_toL1.symm
-  --     _ = const G (⨍ x, f x ∂μ) := congr_arg _ <| average_congr hf.coeFn_toL1
-  -- rcases hf' with ⟨f, rfl⟩; clear hf
