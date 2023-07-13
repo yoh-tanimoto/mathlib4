166ade9b79b01c31a6683db9ab6e00134d33d524
goeken <goeken@uni-bremen.de>
2023-07-13 18:00:00 +0200

    more changes

--- Mathlib/Analysis/Complex/UpperHalfPlane/FuchsianGroup.lean
+++ Mathlib/Analysis/Complex/UpperHalfPlane/FuchsianGroup.lean
@@ -1,6 +1,6 @@
 import Mathlib.Analysis.Complex.UpperHalfPlane.Metric
 
-open MatrixGroups Matrix
+open MatrixGroups Matrix SpecialLinearGroup
 
 instance (n R : Type _) [Fintype n] [DecidableEq n] [CommRing R] [TopologicalSpace R] :
     TopologicalSpace (SpecialLinearGroup n R) := by
@@ -11,11 +11,55 @@ instance (n R : Type _) [Fintype n] [DecidableEq n] [CommRing R] [TopologicalSpa
 
 lemma mem_center_SpecialLinearGroup_two_iff {A : SL(2,ℝ)} :
     A ∈ Subgroup.center SL(2,ℝ) ↔ A = (1 : Matrix (Fin 2) (Fin 2) ℝ) ∨ A = (-1 : Matrix (Fin 2) (Fin 2) ℝ) := by
+    rw [Subgroup.mem_center_iff]
+    rw [coe_mk]
+    swap
+    · simp    
+    constructor
+    swap
+    intro h hg
+    --apply h
+    · sorry
+    intro h
+    
+
+    --have h₁ :  ↑A = 1 ∨ ↑A = -1 → (∀ (g : SL(2, ℝ)), g * A = A * g) := by
+    left
+
+    --have h₂ :  (∀ (g : SL(2, ℝ)), g * A = A * g) → (↑A = 1 ∨ ↑A = -1) := by sorry
+    rw [h₂ ]
+    
+
+    --rw [← coe_one]
+    --rw [ext_iff]
+    let S : SL(2, ℝ) := ⟨!![1, 1; 0, 1], by sorry⟩
+    let T : SL(2, ℝ) := ⟨!![1, 0; 1, 1], by sorry⟩
+    let a b : ℝ
+    --have h₁ : (∀ (g : SL(2, ℝ)), g * S = S * g) := by
+      --rw [ext_iff]
+      --sorry
+    --have h₂ : (∀ (g : SL(2, ℝ)), g * T = T * g) := by
+      --sorry
+    --have h₀ : A 1 1 = A 2 2 := by
+      --sorry
+    --have h : A 1 2 = 0 := by
+      --sorry
+    
+
+
+    --Matrix.ext i j
+
+
+    
+    have h₁ : (∀ (g : SL(2, ℝ)), g * S = S * g) := by
+      sorry
+    have h₂ : (∀ (g : SL(2, ℝ)), g * T = T * g) := by
+      sorry
   -- Try using `Subgroup.mem_center_iff` (maybe can `rw`?)
   -- and then special matrices are:
-  let S : SL(2, ℝ) := ⟨!![1, 1; 0, 1], by sorry⟩
-  let T : SL(2, ℝ) := ⟨!![1, 0; 1, 1], by sorry⟩
-  sorry
+
+#check !![1, 1; 0, 1]
+
 
 variable (n R : Type _) [Fintype n] [DecidableEq n] [CommRing R] [TopologicalSpace R]
 
@@ -33,3 +77,6 @@ instance : TopologicalSpace (ProjectiveLinearGroup n R) := by
   infer_instance
 
 variable (G : Subgroup (ProjectiveLinearGroup n ℝ)) [DiscreteTopology G]
+
+#check Subgroup.mem_center_iff
+#check !![1, 1; 0, 1]
