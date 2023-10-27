200adf5bcb9a1cbc51e0bc656641726a2dce7643
Bolton Bailey <bolton.bailey@gmail.com>
2023-10-27 14:25:15 -0500

    unneeded parens

--- Mathlib/Data/Fintype/Pi.lean
+++ Mathlib/Data/Fintype/Pi.lean
@@ -152,8 +152,8 @@ lemma Finset.map_piFinSuccAboveEquiv_filter_piFinset {n : ℕ} {α : Fin (n + 1)
 lemma Finset.filter_piFinset_eq_map_piFinSuccAboveEquiv_symm {n : ℕ} {α : Fin (n + 1) → Type*}
     (p : ((i : Fin n) → α i.succ) → Prop) [DecidablePred p]
     (S : (i : Fin (n + 1)) → Finset (α i)) :
-    ((Fintype.piFinset S).filter (fun r ↦ p (fun x ↦ r <| Fin.succ x)))
-    = ((S 0) ×ˢ (Fintype.piFinset (fun x ↦ S <| Fin.succ x)).filter p).map
+    ((Fintype.piFinset S).filter fun r ↦ p (fun x ↦ r <| Fin.succ x))
+    = (S 0 ×ˢ (Fintype.piFinset fun x ↦ S <| Fin.succ x).filter p).map
         ((Equiv.piFinSuccAboveEquiv α 0).symm.toEmbedding) := by
   rw [← Finset.map_piFinSuccAboveEquiv_filter_piFinset, Finset.map_map,
     Function.Embedding.equiv_toEmbedding_trans_symm_toEmbedding, map_refl]
