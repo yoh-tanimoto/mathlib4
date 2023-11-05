7b2f21d8cae5da5804a01bf0ee361bbdd47484d3
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-11-05 18:11:07 +0100

    golf

--- Mathlib/Sandbox.lean
+++ Mathlib/Sandbox.lean
@@ -16,6 +16,5 @@ theorem MeasureTheory.Measure.prod_noAtoms_snd {α β : Type*} [MeasurableSpace 
 
 instance MeasureTheory.Measure.prod_noAtoms' {α β : Type*} [MeasurableSpace α] [MeasurableSpace β]
     {μ : Measure α} {ν : Measure β} [SigmaFinite ν] [NoAtoms μ] [NoAtoms ν] :
-    NoAtoms (Measure.prod μ ν) := by
-  refine NoAtoms.mk (fun x => ?_)
-  rw [← Set.singleton_prod_singleton, Measure.prod_prod, measure_singleton, zero_mul]
+    NoAtoms (Measure.prod μ ν) := MeasureTheory.Measure.prod_noAtoms_fst
+
