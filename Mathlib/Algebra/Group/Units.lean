d47813cc96960175948fad0e1ce8f981846f9f8d
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-01-13 11:44:50 +0100

    chore: move units_eq_one earlier

--- Mathlib/Algebra/Group/Units.lean
+++ Mathlib/Algebra/Group/Units.lean
@@ -702,6 +702,10 @@ lemma IsUnit.exists_left_inv [Monoid M] {a : M} (h : IsUnit a) : ∃ b, b * a = 
 #align is_unit.pow IsUnit.pow
 #align is_add_unit.nsmul IsAddUnit.nsmul
 
+theorem units_eq_one [Unique Mˣ] (u : Mˣ) : u = 1 :=
+  Subsingleton.elim u 1
+#align units_eq_one units_eq_one
+
 end Monoid
 
 @[to_additive]
