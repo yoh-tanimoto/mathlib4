c349d6b3567a8f002be7f0848f019590c7391eda
Adrien Burgun <adrien.burgun@orange.fr>
2024-02-12 15:46:25 +0100

    revert(Topology/Perfect): "prove that a pre-perfect, nonempty, open set is infinite"
    This reverts commit 45b6a9c25780094afc00a7652415ac4ecfb4b0d8.

--- Mathlib/Topology/Separation.lean
+++ Mathlib/Topology/Separation.lean
@@ -838,13 +838,19 @@ theorem IsPreconnected.infinite_of_nontrivial [T1Space X] {s : Set X} (h : IsPre
   exact @PreconnectedSpace.trivial_of_discrete _ _ (Subtype.preconnectedSpace h) _
 #align is_preconnected.infinite_of_nontrivial IsPreconnected.infinite_of_nontrivial
 
-/--
-Prefer `PerfectSpace.infinite`, which is a strictly broader theorem.
--/
-@[deprecated]
 theorem ConnectedSpace.infinite [ConnectedSpace X] [Nontrivial X] [T1Space X] : Infinite X :=
   infinite_univ_iff.mp <| isPreconnected_univ.infinite_of_nontrivial nontrivial_univ
 #align connected_space.infinite ConnectedSpace.infinite
+
+/-- A non-trivial connected T1 space has no isolated points. -/
+instance (priority := 100) ConnectedSpace.neBot_nhdsWithin_compl_of_nontrivial_of_t1space
+    [ConnectedSpace X] [Nontrivial X] [T1Space X] (x : X) :
+    NeBot (𝓝[≠] x) := by
+  by_contra contra
+  rw [not_neBot, ← isOpen_singleton_iff_punctured_nhds] at contra
+  replace contra := nonempty_inter isOpen_compl_singleton
+    contra (compl_union_self _) (Set.nonempty_compl_of_nontrivial _) (singleton_nonempty _)
+  simp [compl_inter_self {x}] at contra
 
 theorem singleton_mem_nhdsWithin_of_mem_discrete {s : Set X} [DiscreteTopology s] {x : X}
     (hx : x ∈ s) : {x} ∈ 𝓝[s] x := by
