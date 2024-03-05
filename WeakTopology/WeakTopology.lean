6eb77be8adb55917677470c4ac6aa48c3358b967
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2024-03-05 22:10:12 +0100

    Update WeakTopology.lean

--- WeakTopology/WeakTopology.lean
+++ WeakTopology/WeakTopology.lean
@@ -1,19 +1,12 @@
 import Mathlib
 
-variable  (𝕜 : Type u_8) (E : Type u_9) [CommSemiring 𝕜]
- [TopologicalSpace 𝕜] [ContinuousAdd 𝕜] [ContinuousConstSMul 𝕜 𝕜]
- [AddCommMonoid E] [Module 𝕜 E] [TopologicalSpace E]
+variable {𝕜 : Type*} [NontriviallyNormedField 𝕜]
+variable {E : Type*} [SeminormedAddCommGroup E] [NormedSpace 𝕜 E]
 
-abbrev E_w : Type u_9 := WeakSpace 𝕜 E
 
-#check E_w
-#check WeakSpace.map
--- this only shows that any linear map from E to F is
--- continuous from E_w to F_w. this should be generalized to F
+open NormedSpace
 
--- todo
--- E_w is weaker than the original topology
--- any continuous map from E to any X is continuous from E_w to X
--- for any continuous linear functional ℓ and a convergent net/sequence x n,
--- n → x n is convergent in E_w
--- in particular, in InnerProductSpace
+theorem norm_topology_le_weak_topology :
+    (UniformSpace.toTopologicalSpace : TopologicalSpace E) ≤
+      (WeakSpace.instTopologicalSpace : TopologicalSpace (WeakSpace 𝕜 E)) :=
+      sorry
