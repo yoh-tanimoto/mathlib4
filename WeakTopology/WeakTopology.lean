9aeb1bf0bf6fb65010b31c0e4efab081625d831f
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2024-03-06 09:45:58 +0100

    Update WeakTopology.lean

--- WeakTopology/WeakTopology.lean
+++ WeakTopology/WeakTopology.lean
@@ -6,7 +6,16 @@ variable {E : Type*} [SeminormedAddCommGroup E] [NormedSpace 𝕜 E]
 
 open NormedSpace
 
+
 theorem norm_topology_le_weak_topology :
-    (UniformSpace.toTopologicalSpace : TopologicalSpace E) ≤
-      (WeakSpace.instTopologicalSpace : TopologicalSpace (WeakSpace 𝕜 E)) :=
-      sorry
+(UniformSpace.toTopologicalSpace : TopologicalSpace E) ≤
+  (WeakSpace.instTopologicalSpace : TopologicalSpace (WeakSpace 𝕜 E)) := by
+  apply Continuous.le_induced
+  refine continuous_pi ?h.h
+  intro y
+  simp
+  have : (fun x => ((topDualPairing 𝕜 E) y) x) = (fun x => y x) := by
+   ext x
+   exact topDualPairing_apply y x
+  rw [this]
+  exact ContinuousLinearMap.continuous y
