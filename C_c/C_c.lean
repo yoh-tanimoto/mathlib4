207bcdf5528153cfa9159267609e8d49905451d6
Yoh Tanimoto <57562556+yoh-tanimoto@users.noreply.github.com>
2024-03-30 11:29:25 +0100

    Update C_c.lean

--- C_c/C_c.lean
+++ C_c/C_c.lean
@@ -12,37 +12,53 @@ open Filter Metric
 
 open ZeroAtInfty
 
-/-- `C_c(α, β)` is the type of continuous functions `α → β` which have compact support from a
-topological space to a metric space with a zero element.
+-- /-- `C_c(α, β)` is the type of continuous functions `α → β` which have compact support from a
+-- topological space to a metric space with a zero element.
 
-When possible, instead of parametrizing results over `(f : C_c(α, β))`,
-you should parametrize over `(F : Type*) [CompactlySupportedContinuousMap F α β] (f : F)`.
+-- When possible, instead of parametrizing results over `(f : C_c(α, β))`,
+-- you should parametrize over `(F : Type*) [CompactlySupportedContinuousMap F α β] (f : F)`.
 
-When you extend this structure, make sure to extend `CompactlySupportedContinuousMap`. -/
-structure CompactlySupportedContinuousMap (α : Type u) (β : Type v) [TopologicalSpace α] [Zero β]
-    [TopologicalSpace β] extends ZeroAtInftyContinuousMap α β : Type max u v where
-  hasCompactSupport' : HasCompactSupport toFun
+-- When you extend this structure, make sure to extend `CompactlySupportedContinuousMap`. -/
+-- structure CompactlySupportedContinuousMap (α : Type u) (β : Type v) [TopologicalSpace α] [Zero β]
+--     [TopologicalSpace β] extends ZeroAtInftyContinuousMap α β : Type max u v where
+--   hasCompactSupport' : HasCompactSupport toFun
 
-@[inherit_doc]
-scoped[CompactlySupported] notation (priority := 2000) "C_c(" α ", " β ")" =>
-    CompactlySupportedContinuousMap α β
+-- @[inherit_doc]
+-- scoped[CompactlySupported] notation (priority := 2000) "C_c(" α ", " β ")" =>
+--     CompactlySupportedContinuousMap α β
 
-@[inherit_doc]
-scoped[CompactlySupported] notation α " →C_c " β => CompactlySupportedContinuousMap α β
+-- @[inherit_doc]
+-- scoped[CompactlySupported] notation α " →C_c " β => CompactlySupportedContinuousMap α β
 
-open CompactlySupported
+-- open CompactlySupported
 
-section
+-- section
 
-/-- `CompactlySupportedContinuousMapClass F α β` states that `F` is a type of continuous maps which
-have compact support.
+-- /-- `CompactlySupportedContinuousMapClass F α β` states that `F` is a type of continuous maps which
+-- have compact support.
 
-You should also extend this typeclass when you extend `CompactlySupportedContinuousMap`. -/
-class CompactlySupportedContinuousMapClass (F : Type*) (α β : outParam <| Type*)
-    [TopologicalSpace α] [Zero β] [TopologicalSpace β] [FunLike F α β] extends
-    ZeroAtInftyContinuousMapClass F α β : Prop where
-  /-- Each member of the class has compact support. -/
-  hasCompactSupport (f : F): HasCompactSupport f
+-- You should also extend this typeclass when you extend `CompactlySupportedContinuousMap`. -/
+-- class CompactlySupportedContinuousMapClass (F : Type*) (α β : outParam <| Type*)
+--     [TopologicalSpace α] [Zero β] [TopologicalSpace β] [FunLike F α β] extends
+--     ZeroAtInftyContinuousMapClass F α β : Prop where
+--   /-- Each member of the class has compact support. -/
+--   hasCompactSupport (f : F) : HasCompactSupport f
+
+
+
+-- namespace CompactlySupportedContinuousMap
+
+-- section Basics
+
+-- variable [TopologicalSpace β] [Zero β] [FunLike F α β] [CompactlySupportedContinuousMapClass F α β]
+
+-- instance instFunLike : FunLike C_c(α, β) α β where
+--   coe f := f.toFun
+--   coe_injective' f g h := by
+--     obtain ⟨⟨_, _⟩, _⟩ := f
+--     obtain ⟨⟨_, _⟩, _⟩ := g
+--     congr
+--     exact ContinuousMap.ext (congrFun h)
 
 variable {X : Type*} [TopologicalSpace X] [LocallyCompactSpace X]
  [T2Space X]
@@ -69,25 +85,116 @@ example (g : C(X, 𝕂)) (hG : HasCompactSupport g)
  intro x
  rfl
 
-variable (f1 : C₀(X, ℂ)) (f2 : X →ᵇ ℝ)
-variable (x1 : 𝕂) (x2 : ℝ)
-#check f1 * f1
-#check f2 • f1.toBCF
 
-#check f1 * ((f2 ·) : X → ℂ)
-def f3 := fun x => f1 x * f2 x
-#check f3 f1 f2
-#check x1 * x2
+
+
 
 lemma zero_at_infty_mul_BCF_ZeroAtInfty (f : X →ᵇ ℝ) (g : C₀(X, ℝ)) :
-    Filter.Tendsto (f * g.toBCF) (Filter.cocompact X) (nhds 0):= by
-  sorry
+    Filter.Tendsto (f * g.toBCF) (Filter.cocompact X) (nhds 0) := by
+  have : Filter.Tendsto (fun x => g.toBCF x) (Filter.cocompact X) (nhds 0) := by
+    exact g.2
+  have : Filter.Tendsto (fun x => ‖g.toBCF x‖) (Filter.cocompact X) (nhds ‖(0 : ℝ)‖) := by
+    exact Filter.Tendsto.norm this
+  have : Filter.Tendsto (fun x => ‖f‖ * ‖g.toBCF x‖) (Filter.cocompact X) (nhds (‖f‖ * ‖(0 : ℝ)‖)) := by
+    exact Tendsto.const_mul ‖f‖ this
+  rw [norm_zero, mul_zero] at this
+  apply squeeze_zero_norm _ this
+  intro x
+  have h : (f * g.toBCF) x = (f x) * (g.toBCF x) := by
+    exact rfl
+  rw [h]
+  calc ‖f x * (g.toBCF) x‖ ≤ ‖f x‖ * ‖(g.toBCF) x‖ := by exact norm_mul_le _ _
+  _ ≤  ‖f‖ * ‖(g.toBCF) x‖ := by exact mul_le_mul_of_nonneg_right (BoundedContinuousFunction.norm_coe_le_norm f x) (norm_nonneg ((g.toBCF) x))
 
 
--- helped by Filippo Nuccio
-lemma exist_HasCompactSupport_and_Tendsto' (f : C₀(X, ℂ)) : ∃ (g : ℕ → C₀(X ,ℂ)),
+
+-- -- helped by Filippo Nuccio
+-- lemma exist_HasCompactSupport_and_Tendsto' (f : C₀(X, ℂ)) : ∃ (g : ℕ → C₀(X ,ℂ)),
+--     (∀ (n : ℕ), HasCompactSupport (g n)) ∧ Filter.Tendsto g Filter.atTop (nhds f) := by
+--  have h : ∀ (n : ℕ), ∃ (gn : C₀(X, ℂ)), HasCompactSupport gn ∧ ‖f - gn‖ ≤ 1/((n : ℝ)+1) := by
+--   intro n
+--   have h1 : {x : X | dist (f x) 0 < 1/((n : ℝ)+1)} ∈ Filter.cocompact X := by
+--    apply Filter.eventually_iff.mp
+--    apply Metric.tendsto_nhds.mp (ZeroAtInftyContinuousMap.zero_at_infty' f)
+--    exact Nat.one_div_pos_of_nat
+--   rw [Filter.mem_cocompact] at h1
+--   obtain ⟨K, hK⟩ := h1
+--   rw [Set.compl_subset_comm] at hK
+--   obtain ⟨U, hU⟩ := exists_isOpen_superset_and_isCompact_closure hK.left
+--   obtain ⟨k, hk⟩ := exists_continuous_one_zero_of_isCompact hK.left (IsOpen.isClosed_compl hU.left) (LE.le.disjoint_compl_right hU.right.left)
+--   have hkcp : HasCompactSupport (ContinuousMap.comp ⟨Complex.ofRealCLM, Complex.ofRealCLM.cont⟩ k) := by
+--    have hk1 : Complex.ofRealCLM 0 = 0 := by rfl
+--    have hk2 : Function.support (ContinuousMap.comp ⟨Complex.ofRealCLM, Complex.ofRealCLM.cont⟩ k) ⊆ Function.support k := by
+--     apply Function.support_comp_subset hk1
+--    unfold HasCompactSupport
+--    exact IsCompact.closure_of_subset hk.right.right.left (subset_trans hk2 subset_closure)
+
+--   let gn : C₀(X, ℂ)
+--     := ⟨f.1 * (ContinuousMap.comp ⟨Complex.ofRealCLM, Complex.ofRealCLM.cont⟩ k),
+--        (zero_at_infty_of_hasCompactSupport (f.1 * (ContinuousMap.comp
+--        ⟨Complex.ofRealCLM, Complex.ofRealCLM.cont⟩ k)) hkcp.mul_left)⟩
+--   use gn
+--   constructor
+--   exact hkcp.mul_left
+--   have h4 : ∀ (x : X), ‖(f - gn) x‖ ≤ 1 / (↑n + 1) := by
+--    intro x
+--    have h41 : gn x = f x * k x := by rfl
+--    have h42 : (f - gn) x = f x - gn x := by rfl
+--    rw [h42, h41]
+--    have h43 : 0 ≤ k x ∧ k x ≤ 1 := by
+--     apply hk.right.right.right x
+--    have h44 : 0 ≤ 1 - k x ∧ 1 - k x ≤ 1 := by
+--     constructor
+--     nth_rw 1 [← sub_self 1]
+--     exact (sub_le_sub (le_refl 1) h43.right)
+--     nth_rw 2 [← sub_zero 1]
+--     exact (sub_le_sub (le_refl 1) h43.left)
+--    have h441 : |1 - k x| ≤ |1| := by
+--     exact abs_le_abs_of_nonneg h44.1 h44.2
+--    rw [abs_one] at h441
+--    have h45 : f x - f x * k x = f x * (1 - k x) := by ring
+--    rw [h45]
+--    by_cases hxK : x ∈ K
+--    have h46 : k x = 1 := by exact (hk.1 hxK)
+--    rw [h46]
+--    simp
+--    apply le_of_lt (Nat.cast_add_one_pos n)
+--    rw [Set.compl_subset_comm] at hK
+--    have h47 : dist (f x) 0 < 1/(n+1) := by
+--     apply hK.2
+--     exact Set.mem_compl hxK
+--    rw [SeminormedAddCommGroup.dist_eq, sub_zero] at h47
+--    rw [norm_mul, mul_comm]
+--    apply mul_le_mul
+--    rw [← Complex.abs_ofReal, ← Complex.norm_eq_abs] at h441
+--    have h471 : (1 - (k x : ℂ)) = ((1 - k x) : ℝ) := by simp
+--    rw [← h471] at h441
+--    exact h441
+--    rw [← one_div]
+--    exact le_of_lt h47
+--    simp
+--    norm_num
+--   apply (BoundedContinuousFunction.norm_le (le_of_lt Nat.one_div_pos_of_nat)).mpr h4
+--  obtain ⟨g, hg⟩ := Classical.axiomOfChoice h
+--  use g
+--  constructor
+--  intro n
+--  exact (hg n).left
+--  rw [Metric.tendsto_atTop]
+--  intro ε hε
+--  use Nat.ceil (1/ε)
+--  intro n hn
+--  rw [dist_comm, SeminormedAddCommGroup.dist_eq]
+--  apply Nat.le_of_ceil_le at hn
+--  have h5 : 1 / (n+1) < ε := by
+--   rw [(one_div_lt (Nat.cast_add_one_pos n) hε)]
+--   exact lt_of_le_of_lt hn (lt_add_one (n : ℝ))
+--  exact lt_of_le_of_lt (hg n).right h5
+
+
+lemma exist_HasCompactSupport_and_Tendsto (f : C₀(X, 𝕂)) : ∃ (g : ℕ → C₀(X ,𝕂)),
     (∀ (n : ℕ), HasCompactSupport (g n)) ∧ Filter.Tendsto g Filter.atTop (nhds f) := by
- have h : ∀ (n : ℕ), ∃ (gn : C₀(X, ℂ)), HasCompactSupport gn ∧ ‖f - gn‖ ≤ 1/((n : ℝ)+1) := by
+ have h : ∀ (n : ℕ), ∃ (gn : C₀(X, 𝕂)), HasCompactSupport gn ∧ ‖f - gn‖ ≤ 1/((n : ℝ)+1) := by
   intro n
   have h1 : {x : X | dist (f x) 0 < 1/((n : ℝ)+1)} ∈ Filter.cocompact X := by
    apply Filter.eventually_iff.mp
@@ -98,17 +205,16 @@ lemma exist_HasCompactSupport_and_Tendsto' (f : C₀(X, ℂ)) : ∃ (g : ℕ →
   rw [Set.compl_subset_comm] at hK
   obtain ⟨U, hU⟩ := exists_isOpen_superset_and_isCompact_closure hK.left
   obtain ⟨k, hk⟩ := exists_continuous_one_zero_of_isCompact hK.left (IsOpen.isClosed_compl hU.left) (LE.le.disjoint_compl_right hU.right.left)
-  have hkcp : HasCompactSupport (ContinuousMap.comp ⟨Complex.ofRealCLM, Complex.ofRealCLM.cont⟩ k) := by
-   have hk1 : Complex.ofRealCLM 0 = 0 := by rfl
-   have hk2 : Function.support (ContinuousMap.comp ⟨Complex.ofRealCLM, Complex.ofRealCLM.cont⟩ k) ⊆ Function.support k := by
-    apply Function.support_comp_subset hk1
+  have hkcp : HasCompactSupport (ContinuousMap.comp ⟨(IsROrC.ofRealCLM : ℝ →L[ℝ] 𝕂), IsROrC.ofRealCLM.cont⟩ k) := by
+   have hk2 : Function.support (ContinuousMap.comp ⟨(IsROrC.ofRealCLM : ℝ →L[ℝ] 𝕂), IsROrC.ofRealCLM.cont⟩ k) ⊆ Function.support k := by
+    apply Function.support_comp_subset IsROrC.ofReal_zero
    unfold HasCompactSupport
    exact IsCompact.closure_of_subset hk.right.right.left (subset_trans hk2 subset_closure)
 
-  let gn : C₀(X, ℂ)
-    := ⟨f.1 * (ContinuousMap.comp ⟨Complex.ofRealCLM, Complex.ofRealCLM.cont⟩ k),
+  let gn : C₀(X, 𝕂)
+    := ⟨f.1 * (ContinuousMap.comp ⟨(IsROrC.ofRealCLM : ℝ →L[ℝ] 𝕂), IsROrC.ofRealCLM.cont⟩ k),
        (zero_at_infty_of_hasCompactSupport (f.1 * (ContinuousMap.comp
-       ⟨Complex.ofRealCLM, Complex.ofRealCLM.cont⟩ k)) hkcp.mul_left)⟩
+       ⟨(IsROrC.ofRealCLM : ℝ →L[ℝ] 𝕂), IsROrC.ofRealCLM.cont⟩ k)) hkcp.mul_left)⟩
   use gn
   constructor
   exact hkcp.mul_left
@@ -127,7 +233,7 @@ lemma exist_HasCompactSupport_and_Tendsto' (f : C₀(X, ℂ)) : ∃ (g : ℕ →
     exact (sub_le_sub (le_refl 1) h43.left)
    have h441 : |1 - k x| ≤ |1| := by
     exact abs_le_abs_of_nonneg h44.1 h44.2
-   rw [abs_one] at h441
+   rw [abs_one, ← Real.norm_eq_abs] at h441
    have h45 : f x - f x * k x = f x * (1 - k x) := by ring
    rw [h45]
    by_cases hxK : x ∈ K
@@ -142,9 +248,9 @@ lemma exist_HasCompactSupport_and_Tendsto' (f : C₀(X, ℂ)) : ∃ (g : ℕ →
    rw [SeminormedAddCommGroup.dist_eq, sub_zero] at h47
    rw [norm_mul, mul_comm]
    apply mul_le_mul
-   rw [← Complex.abs_ofReal, ← Complex.norm_eq_abs] at h441
-   have h471 : (1 - (k x : ℂ)) = ((1 - k x) : ℝ) := by simp
-   rw [← h471] at h441
+   have h471 : (1 - (k x : 𝕂)) = ((1 - k x) : ℝ) := by simp
+   rw [h471]
+   rw [IsROrC.norm_ofReal]
    exact h441
    rw [← one_div]
    exact le_of_lt h47
@@ -166,85 +272,3 @@ lemma exist_HasCompactSupport_and_Tendsto' (f : C₀(X, ℂ)) : ∃ (g : ℕ →
   rw [(one_div_lt (Nat.cast_add_one_pos n) hε)]
   exact lt_of_le_of_lt hn (lt_add_one (n : ℝ))
  exact lt_of_le_of_lt (hg n).right h5
-
-lemma exist_HasCompactSupport_and_Tendsto (f : C₀(X, 𝕂)) : ∃ (g : ℕ → C₀(X ,𝕂)),
-    (∀ (n : ℕ), HasCompactSupport (g n)) ∧ Filter.Tendsto g Filter.atTop (nhds f) := by
- have h : ∀ (n : ℕ), ∃ (gn : C₀(X, 𝕂)), HasCompactSupport gn ∧ ‖f - gn‖ ≤ 1/((n : ℝ)+1) := by
-  intro n
-  have h1 : {x : X | dist (f x) 0 < 1/((n : ℝ)+1)} ∈ Filter.cocompact X := by
-   apply Filter.eventually_iff.mp
-   apply Metric.tendsto_nhds.mp (ZeroAtInftyContinuousMap.zero_at_infty' f)
-   exact Nat.one_div_pos_of_nat
-  rw [Filter.mem_cocompact] at h1
-  obtain ⟨K, hK⟩ := h1
-  rw [Set.compl_subset_comm] at hK
-  obtain ⟨U, hU⟩ := exists_isOpen_superset_and_isCompact_closure hK.left
-  obtain ⟨k, hk⟩ := exists_continuous_one_zero_of_isCompact hK.left (IsOpen.isClosed_compl hU.left) (LE.le.disjoint_compl_right hU.right.left)
-  have hkcp : HasCompactSupport (ContinuousMap.comp ⟨Complex.ofRealCLM, Complex.ofRealCLM.cont⟩ k) := by
-   have hk1 : Complex.ofRealCLM 0 = 0 := by rfl
-   have hk2 : Function.support (ContinuousMap.comp ⟨Complex.ofRealCLM, Complex.ofRealCLM.cont⟩ k) ⊆ Function.support k := by
-    apply Function.support_comp_subset hk1
-   unfold HasCompactSupport
-   exact IsCompact.closure_of_subset hk.right.right.left (subset_trans hk2 subset_closure)
-
-  let gn : C₀(X, 𝕂)
-    := ⟨f.1 * k, zero_at_infty_of_hasCompactSupport (f.1 * k), hkcp.mul_left⟩
-  use gn
-  constructor
-  sorry
-  have h4 : ∀ (x : X), ‖(f - gn) x‖ ≤ 1 / (↑n + 1) := by
-   intro x
-   have h41 : gn x = f x * k x := by rfl
-   have h42 : (f - gn) x = f x - gn x := by rfl
-   rw [h42, h41]
-   have h43 : 0 ≤ k x ∧ k x ≤ 1 := by
-    apply hk.right.right.right x
-   have h44 : 0 ≤ 1 - k x ∧ 1 - k x ≤ 1 := by
-    constructor
-    nth_rw 1 [← sub_self 1]
-    exact (sub_le_sub (le_refl 1) h43.right)
-    nth_rw 2 [← sub_zero 1]
-    exact (sub_le_sub (le_refl 1) h43.left)
-   have h441 : |1 - k x| ≤ |1| := by
-    exact abs_le_abs_of_nonneg h44.1 h44.2
-   rw [abs_one] at h441
-   have h45 : f x - f x * k x = f x * (1 - k x) := by ring
-   rw [h45]
-   by_cases hxK : x ∈ K
-   have h46 : k x = 1 := by exact (hk.1 hxK)
-   rw [h46]
-   simp
-   apply le_of_lt (Nat.cast_add_one_pos n)
-   rw [Set.compl_subset_comm] at hK
-   have h47 : dist (f x) 0 < 1/(n+1) := by
-    apply hK.2
-    exact Set.mem_compl hxK
-   rw [SeminormedAddCommGroup.dist_eq, sub_zero] at h47
-   rw [norm_mul, mul_comm]
-   apply mul_le_mul
-   rw [← Complex.abs_ofReal, ← Complex.norm_eq_abs] at h441
-   have h471 : (1 - (k x : ℂ)) = ((1 - k x) : ℝ) := by simp
-   rw [← h471] at h441
-   exact h441
-   rw [← one_div]
-   exact le_of_lt h47
-   simp
-   norm_num
-  apply (BoundedContinuousFunction.norm_le (le_of_lt Nat.one_div_pos_of_nat)).mpr h4
- obtain ⟨g, hg⟩ := Classical.axiomOfChoice h
- use g
- constructor
- intro n
- exact (hg n).left
- rw [Metric.tendsto_atTop]
- intro ε hε
- use Nat.ceil (1/ε)
- intro n hn
- rw [dist_comm, SeminormedAddCommGroup.dist_eq]
- apply Nat.le_of_ceil_le at hn
- have h5 : 1 / (n+1) < ε := by
-  rw [(one_div_lt (Nat.cast_add_one_pos n) hε)]
-  exact lt_of_le_of_lt hn (lt_add_one (n : ℝ))
- exact lt_of_le_of_lt (hg n).right h5
-
-end
