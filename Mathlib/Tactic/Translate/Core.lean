yoh-tanimoto/mathlib4

commit 1398533ad13f4843da7e2f946d2c1b141800bc1f

feat: unfold-boundaries in `to_dual` (#32438)
This PR introduces a new feature to `to_dual` that allows us to have definitions whose dual is not definitionally equal to the dual of the value. This is crucial for many definitions that are dual to something morally but not definitionally. For example, `DecidableLE`, `Set.Ioo`, `Set.Ico`, `CovBy` , `Monotone`.

This adds 2 new files:
- `Mathlib.Tactic.Translate.UnfoldBoundary` contains the code for adding the casts into expressions
- `Mathlib.Tactic.Translate.TagUnfoldBoundary` contains the code for registering constants as needing a cast.

Mathlib/Tactic/Translate/Core.lean
@@ -19,6 +19,7 @@ public import Batteries.Tactic.Trans
 public import Mathlib.Tactic.Eqns
 public import Mathlib.Tactic.Simps.Basic
 public import Mathlib.Tactic.Translate.GuessName
+public import Mathlib.Tactic.Translate.UnfoldBoundary
 public meta import Mathlib.Util.MemoFix
 
 /-!
@@ -179,6 +180,11 @@ structure TranslateData : Type where
     translated thanks to this, you generally have to specify the translated name manually.
   -/
   doTranslateAttr : NameMapExtension Bool
+  /-- The `insert_cast`/`insert_cast_fun` attributes create an abstraction boundary for the tagged
+  constant when translating it. For example, `Set.Icc`, `Monotone`, `DecidableLT`, `WCovBy` are all
+  morally self-dual, but their definition is not self-dual. So, in order to allow these constants
+  to be self-dual, we need to not unfold their definition in the proof term that we translate. -/
+  unfoldBoundaries? : Option UnfoldBoundary.UnfoldBoundaryExt := none
   /-- `translations` stores all of the constants that have been tagged with this attribute,
   and maps them to their translation. -/
   translations : NameMapExtension TranslationInfo
@@ -560,11 +566,21 @@ def updateDecl (t : TranslateData) (tgt : Name) (srcDecl : ConstantInfo)
   let mut decl := srcDecl.updateName tgt
   if reorder.any (·.contains 0) then
     decl := decl.updateLevelParams decl.levelParams.swapFirstTwo
-  decl := decl.updateType <| ← reorderForall reorder <| ← applyReplacementForall t dont <|
-    renameBinderNames t decl.type
-  if let some v := decl.value? (allowOpaque := true) then
-    decl := decl.updateValue <| ← reorderLambda reorder <| ← applyReplacementLambda t dont v
-  return decl
+  let mut value := decl.value! (allowOpaque := true)
+  if let some b := t.unfoldBoundaries? then
+    value ← b.cast (← b.insertBoundaries value t.attrName) decl.type t.attrName
+  trace[translate] "Value before translation:{indentExpr value}"
+  value ← reorderLambda reorder <| ← applyReplacementLambda t dont value
+  if let some b := t.unfoldBoundaries? then
+    value ← b.unfoldInsertions value
+  decl := decl.updateValue value
+  let mut type := decl.type
+  if let some b := t.unfoldBoundaries? then
+    type ← b.insertBoundaries decl.type t.attrName
+  type ← reorderForall reorder <| ← applyReplacementForall t dont <| renameBinderNames t type
+  if let some b := t.unfoldBoundaries? then
+    type ← b.unfoldInsertions type
+  return decl.updateType type
 
 /--
 Find the argument of `nm` that appears in the first translatable (type-class) argument.
@@ -601,9 +617,7 @@ def declUnfoldSimpAuxLemmas (decl : ConstantInfo) : MetaM ConstantInfo := do
   let mut decl := decl
   decl := decl.updateType <| ← unfold decl.type
   if let some v := decl.value? (allowOpaque := true) then
-    trace[translate] "value before unfold:{indentExpr v}"
     decl := decl.updateValue <| ← unfold v
-    trace[translate] "value after unfold:{indentExpr decl.value!}"
   return decl
 
 /-- Find the target name of `src`, which is assumed to have been selected by `findAuxDecls`. -/
@@ -898,7 +912,10 @@ partial def checkExistingType (t : TranslateData) (src tgt : Name) (cfg : Config
   unless srcDecl.levelParams.length == tgtDecl.levelParams.length do
     throwError "`{t.attrName}` validation failed:\n  expected {srcDecl.levelParams.length} \
       universe levels, but '{tgt}' has {tgtDecl.levelParams.length} universe levels"
-  let srcType ← applyReplacementForall t cfg.dontTranslate srcDecl.type
+  let mut srcType := srcDecl.type
+  if let some b := t.unfoldBoundaries? then
+    srcType ← b.insertBoundaries srcType t.attrName
+  srcType ← applyReplacementForall t cfg.dontTranslate srcType
   let reorder' := guessReorder srcType tgtDecl.type
   trace[translate_detail] "The guessed reorder is {reorder'}"
   let reorder ←
@@ -920,12 +937,14 @@ partial def checkExistingType (t : TranslateData) (src tgt : Name) (cfg : Config
       Please remove the attribute, or provide an explicit `(reorder := ...)` argument.\n\
       If you need to give a hint to `{t.attrName}` to translate expressions involving `{src}`,\n\
       use `{t.attrName}_do_translate` instead"
-  let srcType ← reorderForall reorder srcType
+  srcType ← reorderForall reorder srcType
+  if let some b := t.unfoldBoundaries? then
+    srcType ← b.unfoldInsertions srcType
   if reorder.any (·.contains 0) then
     srcDecl := srcDecl.updateLevelParams srcDecl.levelParams.swapFirstTwo
   -- instantiate both types with the same universes. `instantiateLevelParams` does some
   -- normalization, so we apply it to both types.
-  let srcType := srcType.instantiateLevelParams
+  srcType := srcType.instantiateLevelParams
     srcDecl.levelParams (tgtDecl.levelParams.map mkLevelParam)
   let tgtType := tgtDecl.type.instantiateLevelParams
     tgtDecl.levelParams (tgtDecl.levelParams.map mkLevelParam)

Mathlib/Tactic/Translate/UnfoldBoundary.lean
@@ -1,0 +1,200 @@
+/-
+Copyright (c) 2025 Jovan Gerbscheid. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jovan Gerbscheid
+-/
+module
+
+public meta import Lean.Meta.Tactic.Delta
+public import Batteries.Lean.NameMapAttribute
+public import Mathlib.Init
+
+/-!
+# Modify proof terms so that they don't rely on unfolding certain constants
+
+This file defines a procedure for inserting casts into (proof) terms in order to make them
+well typed in a setting where certain constants aren't allowed to be unfolded.
+
+We make use of `withCanUnfoldPred` in order to modify which constants can and cannot be unfolded.
+This way, `whnf` and `isDefEq` do not unfold these constants.
+
+So, the procedure is to check that an expression is well typed, analogous to `Meta.check`,
+and at each type mismatch, we try to insert a cast.
+
+There are two kinds of casts:
+- Equality casts. This is for propositions and terms,
+  where it is possible to prove that one is equal to the other. For example `Monotone`.
+- Explicit casting functions, both for unfolding and refolding. This is for types, where we
+  cannot express their equivalence with an equality. For example `DecidableLE`.
+-/
+
+meta section
+
+namespace Mathlib.Tactic.UnfoldBoundary
+
+open Lean Meta
+
+/-- `UnfoldBoundaries` stores abstraction boundaries for definitions that shouldn't be unfolded. -/
+public structure UnfoldBoundaries where
+  /-- For propositions and terms of types, we store a rewrite theorem that unfolds it. -/
+  unfolds : NameMap SimpTheorem := {}
+  /-- For types, we store a cast for translating from and to the type respectively. -/
+  casts : NameMap (Name × Name) := {}
+  /-- The functions that we want to unfold again after the translation has happened. -/
+  insertionFuns : NameSet := {}
+  deriving Inhabited
+
+/--
+Set up the monadic context:
+- Set the transparency to `.all`, just like is done in `Meta.check`.
+- Use `withCanUnfoldPred` to not allow unfolding the constants for which we want to insert casts.
+- Set up the `SimpM` context so that `Simp.simp` will unfold constants from `b.unfolds`.
+-/
+def run {α} (b : UnfoldBoundaries) (x : SimpM α) : MetaM α :=
+  withCanUnfoldPred (fun _ i => return !b.unfolds.contains i.name && !b.casts.contains i.name) do
+  withTransparency .all do
+  let ctx ← Simp.mkContext Simp.neutralConfig
+  x (Simp.Methods.toMethodsRef { pre }) ctx |>.run' {}
+where
+  pre (e : Expr) : SimpM Simp.Step := do
+    let .const c _ ← whnf e.getAppFn | return .continue
+    let some thm := b.unfolds.find? c | return .continue
+    let some r ← Simp.tryTheorem? e thm | return .continue
+    return .visit r
+
+/-- Given a term `e`, add casts to it to unfold constants appearing in it. -/
+partial def unfoldConsts (b : UnfoldBoundaries) (e : Expr) : SimpM Expr := do
+  let eType ← inferType e
+  let e ← do
+    let { expr, proof? := some proof } ← Simp.simp eType | pure e
+    trace[translate_detail] "unfoldConsts: added a cast from {eType} to {expr}"
+    mkAppOptM ``Eq.mp #[eType, expr, proof, e]
+  let eTypeWhnf ← whnf (← inferType e)
+  if let .const c us := eTypeWhnf.getAppFn then
+    if let some (cast, _) := b.casts.find? c then
+      let e := .app (mkAppN (.const cast us) eTypeWhnf.getAppArgs) e
+      trace[translate_detail] "unfoldConsts: created the cast {e} to unfold {.ofConstName c}"
+      return ← unfoldConsts b e
+  return e
+
+/-- Given a term `e` which we want to get to have type `expectedType`, return a term of type
+`expectedType` by adding cast to `e` that unfold constants in `expectedType`. -/
+partial def refoldConsts (b : UnfoldBoundaries) (e expectedType : Expr) : SimpM Expr := do
+  let goal ← mkFreshExprMVar expectedType
+  go e goal.mvarId!
+  instantiateMVars goal
+where
+  go (e : Expr) (goal : MVarId) : SimpM Unit := do
+    let goal ← do
+      let { expr, proof? := some proof } ← Simp.simp (← goal.getType) | pure goal
+      trace[translate_detail] "refoldConsts: added a cast from {← goal.getType} to {expr}"
+      goal.replaceTargetEq expr proof
+    forallTelescope (← goal.getType) fun xs tgt => do
+      let tgt ← whnf tgt
+      if let .const c us := tgt.getAppFn then
+        if let some (_, cast) := b.casts.find? c then
+          let cast := mkAppN (.const cast us) tgt.getAppArgs
+          trace[translate_detail] "refoldConsts: created the cast {cast} to unfold {.ofConstName c}"
+          let .forallE _ α _ _ ← inferType cast | throwError "refoldConsts: not a function\n{cast}"
+          let goal' ← mkFreshExprMVar α
+          go (e.beta xs) goal'.mvarId!
+          goal.assign <| ← mkLambdaFVars xs <| .app cast goal'
+          return
+      unless ← isDefEq (← goal.getType) (← inferType e) do
+        throwError "{e} : {← inferType e} does not have type {← goal.getType}."
+      goal.assign e
+
+/-- Given an expression `e` with expected type `expectedType`, if `e` doesn't have that type,
+use a cast to turn `e` into that type. -/
+def mkCast (b : UnfoldBoundaries) (e expectedType : Expr) : SimpM Expr := do
+  if ← isDefEq (← inferType e) expectedType then
+    return e
+  let e ← unfoldConsts b e
+  refoldConsts b e expectedType
+
+/-- Create the application `.app f a`, inserting some casts if necessary. -/
+def mkAppWithCast (b : UnfoldBoundaries) (f a : Expr) : SimpM Expr :=
+  try
+    checkApp f a
+    return f.app a
+  catch _ =>
+    let f ← unfoldConsts b f
+    let .forallE _ d _ _ ← whnf (← inferType f) | throwFunctionExpected f
+    return f.app (← mkCast b a d)
+
+/-- Modify `e` so that it has type `expectedType` if the constants in `b` cannot be unfolded. -/
+def UnfoldBoundaries.cast (b : UnfoldBoundaries) (e expectedType : Expr) (attr : Name) :
+    MetaM Expr :=
+  run b <|
+  try
+    mkCast b e expectedType
+  catch ex =>
+    throwError "@[{attr}] failed to insert a cast to make `{e}` \
+      have type `{expectedType}`\n\n{ex.toMessageData}"
+
+/-- Modify `e` so that it is well typed if the constants in `b` cannot be unfolded.
+
+Note: it may be that `e` contains some constant whose type is not well typed in this setting.
+We don't make an effort to replace such constants.
+It seems that this approximation works well enough. -/
+def UnfoldBoundaries.insertBoundaries (b : UnfoldBoundaries) (e : Expr) (attr : Name) :
+    MetaM Expr :=
+  run b <| Meta.transform e (post := fun e ↦ e.withApp fun f args =>
+    try
+      return .done <| ← args.foldlM (mkAppWithCast b) f
+    catch ex =>
+      throwError "@[{attr}] failed to insert a cast to make `{f}` applied to `{args.toList}` \
+        well typed\n\n{ex.toMessageData}")
+
+/-- Unfold all of the auxiliary functions that were inserted as unfold boundaries. -/
+def UnfoldBoundaries.unfoldInsertions (e : Expr) (b : UnfoldBoundaries) : CoreM Expr :=
+  -- This is the same as `Meta.deltaExpand`, but with an extra beta reduction.
+  Core.transform e fun e => do
+    if let some e ← delta? e b.insertionFuns.contains then
+      return .visit (headBetaBody e)
+    return .continue
+where
+  headBetaBody (e : Expr) : Expr :=
+    match e with
+    | .lam _ d b bi => e.updateLambda! bi d (headBetaBody b)
+    | _ => e.headBeta
+
+/-- An entry for the `UnfoldBoundaries` environment extension. -/
+public inductive UnfoldEntry where
+  | unfold (declName : Name) (unfold : Name)
+  | cast (declName : Name) (unfold refold unfold' refold' : Name)
+
+def UnfoldBoundaries.insert (b : UnfoldBoundaries) : UnfoldEntry → UnfoldBoundaries
+  | .unfold declName unfold => { b with
+    unfolds := b.unfolds.insert declName
+      { origin := .decl unfold, proof := mkConst unfold, rfl := false } }
+  | .cast declName unfold refold unfold' refold' => { b with
+    casts := b.casts.insert declName (unfold, refold)
+    insertionFuns := b.insertionFuns.insertMany [unfold, refold, unfold', refold'] }
+
+/-- Extensions for handling abstraction boundaries for definitions that shouldn't be unfolded. -/
+public abbrev UnfoldBoundaryExt := SimplePersistentEnvExtension UnfoldEntry UnfoldBoundaries
+
+/-- Register a new `UnfoldBoundaryExt`. -/
+public def registerUnfoldBoundaryExt : IO UnfoldBoundaryExt := do
+  registerSimplePersistentEnvExtension {
+    addEntryFn := UnfoldBoundaries.insert
+    addImportedFn as := as.foldl (Array.foldl (·.insert ·)) {}
+  }
+
+@[inherit_doc UnfoldBoundaries.cast]
+public def UnfoldBoundaryExt.cast (b : UnfoldBoundaryExt) (e expectedType : Expr) (attr : Name) :
+    MetaM Expr := do
+  (b.getState (← getEnv)).cast e expectedType attr
+
+@[inherit_doc UnfoldBoundaries.insertBoundaries]
+public def UnfoldBoundaryExt.insertBoundaries (b : UnfoldBoundaryExt) (e : Expr) (attr : Name) :
+    MetaM Expr := do
+  (b.getState (← getEnv)).insertBoundaries e attr
+
+@[inherit_doc UnfoldBoundaries.unfoldInsertions]
+public def UnfoldBoundaryExt.unfoldInsertions (e : Expr) (b : UnfoldBoundaryExt) : CoreM Expr := do
+  (b.getState (← getEnv)).unfoldInsertions e
+
+
+end Mathlib.Tactic.UnfoldBoundary

commit e3e3ff8b4ae75c666249771d9518d64a031c60b5

perf(to_dual): first try translating without inserting casts (#35131)
This PR addresses the performance regression introduced by #32438. The problem is that deciding where casts need to be inserted is an expensive operation (similarly expensive to kernel type checking). I expect that in category theory this will be particularly significant.

Most uses of `to_dual` don't need any casts to be inserted at all. So, it is more efficient to simply first try translating without inserting casts. And if that fails, we insert casts and then try again.

So after this PR, in the failure case, `to_dual` will check the term 4 times:
- first try the translation without casts
- then determine where casts should go
- then try the translation of this
- then use `Meta.check` to get a nicer error message.

This is OK, since the failure case is not the performance critical case.

Mathlib/Tactic/Translate/Core.lean
@@ -563,25 +563,67 @@ def applyReplacementLambda (t : TranslateData) (dontTranslate : List Nat) (e : E
 
 /-- Run `applyReplacementFun` on the given `srcDecl` to make a new declaration with name `tgt`. -/
 def updateDecl (t : TranslateData) (tgt : Name) (srcDecl : ConstantInfo)
-    (reorder : Reorder) (dont : List Nat) : MetaM ConstantInfo := do
+    (reorder : Reorder) (dont : List Nat)
+    (unfoldBoundaries? : Option UnfoldBoundary.UnfoldBoundaries) : MetaM ConstantInfo := do
   let mut decl := srcDecl.updateName tgt
   if reorder.any (·.contains 0) then
     decl := decl.updateLevelParams decl.levelParams.swapFirstTwo
   let mut value := decl.value! (allowOpaque := true)
-  if let some b := t.unfoldBoundaries? then
+  if let some b := unfoldBoundaries? then
     value ← b.cast (← b.insertBoundaries value t.attrName) decl.type t.attrName
   trace[translate] "Value before translation:{indentExpr value}"
   value ← reorderLambda reorder <| ← applyReplacementLambda t dont value
-  if let some b := t.unfoldBoundaries? then
+  if let some b := unfoldBoundaries? then
     value ← b.unfoldInsertions value
   decl := decl.updateValue value
   let mut type := decl.type
-  if let some b := t.unfoldBoundaries? then
+  if let some b := unfoldBoundaries? then
     type ← b.insertBoundaries decl.type t.attrName
   type ← reorderForall reorder <| ← applyReplacementForall t dont <| renameBinderNames t type
-  if let some b := t.unfoldBoundaries? then
+  if let some b := unfoldBoundaries? then
     type ← b.unfoldInsertions type
   return decl.updateType type
+
+/-- Translate the source declaration and then run `addDecl`. If the kernel throws an error,
+try to emit a better error message.
+
+For efficiency in `to_dual`, we first run `updateDecl` without any `UnfoldBoundaries`,
+and only if that fails do we try to include them.
+The reason is that in the most common case, `to_dual` succeeds without needing to insert
+unfold boundaries, and figuring out whether to insert them can be quite expensive. -/
+def updateAndAddDecl (t : TranslateData) (tgt : Name) (srcDecl : ConstantInfo)
+    (reorder : Reorder) (dont : List Nat) : MetaM ConstantInfo :=
+  -- Set `Elab.async` to `false` so that we can catch kernel errors.
+  withOptions (Elab.async.set · false) do
+  let decl ←
+    if let some unfoldBoundaries := t.unfoldBoundaries? then
+      let env ← getEnv
+      -- First attempt to generate the translation without unfold boundaries.
+      let declAttempt ← updateDecl t tgt srcDecl reorder dont none
+      try
+        addDecl declAttempt.toDeclaration!
+        trace[translate] "generating\n{tgt} : {declAttempt.type} :=\
+          {indentExpr <| declAttempt.value! (allowOpaque := true)}"
+        return declAttempt -- early return
+      catch _ =>
+        setEnv env
+        updateDecl t tgt srcDecl reorder dont (unfoldBoundaries.getState env)
+    else
+      updateDecl t tgt srcDecl reorder dont none
+  trace[translate] "generating\n{tgt} : {decl.type} :=\
+    {indentExpr <| decl.value! (allowOpaque := true)}"
+  try
+    addDecl decl.toDeclaration!
+    return decl
+  catch ex =>
+    try
+      withoutExporting <| check (decl.value! (allowOpaque := true))
+    catch ex =>
+      throwError "@[{t.attrName}] failed to add declaration `{decl.name}`.\n  \
+        The translated value is not type correct.\n  \
+        For help, see the docstring of `to_additive`, section `Troubleshooting`.\n\
+        {ex.toMessageData}"
+    throwError "@[{t.attrName}] failed. Nested error message:\n{ex.toMessageData}"
 
 /--
 Find the argument of `nm` that appears in the first translatable (type-class) argument.
@@ -702,26 +744,10 @@ partial def transformDeclRec (t : TranslateData) (ref : Syntax) (pre tgt_pre src
       let namesSrc := (← getConstInfo src).type.getForallBinderNames
       pure <| dontTranslate.filterMap (namesPre[·]? >>= namesSrc.idxOf?)
   -- now transform the source declaration
-  let trgDecl ← MetaM.run' <| updateDecl t tgt srcDecl reorder dontTranslate
-  if src == pre && srcDecl.isThm && trgDecl.type == srcDecl.type then
+  let tgtDecl ← MetaM.run' <| updateAndAddDecl t tgt srcDecl reorder dontTranslate
+  if src == pre && srcDecl.isThm && tgtDecl.type == srcDecl.type then
     Linter.logLintIf linter.translateRedundant ref m!"`{t.attrName}` did not change the type \
       of theorem `{.ofConstName src}`. Please remove the attribute."
-  let value := trgDecl.value! (allowOpaque := true)
-  trace[translate] "generating\n{tgt} : {trgDecl.type} :=\n  {value}"
-  try
-    -- set `Elab.async` to `false` in order to be able to catch kernel errors
-    withOptions (Elab.async.set · false) do
-      addDecl trgDecl.toDeclaration!
-  catch ex =>
-    -- Try to emit a better error message if the kernel throws an error.
-    try
-      withoutExporting <| MetaM.run' <| check value
-    catch ex =>
-      throwError "@[{t.attrName}] failed. \
-        The translated value is not type correct. For help, see the docstring \
-        of `to_additive`, section `Troubleshooting`. \
-        Failed to add declaration\n{tgt}:\n{ex.toMessageData}"
-    throwError "@[{t.attrName}] failed. Nested error message:\n{ex.toMessageData}"
   /- If `src` is explicitly marked as `noncomputable`, then add the new decl as a declaration but
   do not compile it, and mark is as noncomputable. Otherwise, only log errors in compiling if `src`
   has executable code.
@@ -740,8 +766,8 @@ partial def transformDeclRec (t : TranslateData) (ref : Syntax) (pre tgt_pre src
     if isMarkedMeta (← getEnv) src then
       -- We need to mark `tgt` as `meta` before running `compileDecl`
       modifyEnv (markMeta · tgt)
-    compileDecl trgDecl.toDeclaration! (logErrors := (IR.findEnvDecl (← getEnv) src).isSome)
-  if let .defnInfo { hints := .abbrev, .. } := trgDecl then
+    compileDecl tgtDecl.toDeclaration! (logErrors := (IR.findEnvDecl (← getEnv) src).isSome)
+  if let .defnInfo { hints := .abbrev, .. } := tgtDecl then
     if (← getReducibilityStatus src) == .reducible then
       setReducibilityStatus tgt .reducible
     if Compiler.getInlineAttribute? (← getEnv) src == some .inline then
@@ -917,7 +943,8 @@ partial def checkExistingType (t : TranslateData) (src tgt : Name) (cfg : Config
     throwError "`{t.attrName}` validation failed:\n  expected {srcDecl.levelParams.length} \
       universe levels, but '{tgt}' has {tgtDecl.levelParams.length} universe levels"
   let mut srcType := srcDecl.type
-  if let some b := t.unfoldBoundaries? then
+  let unfoldBoundaries? ← t.unfoldBoundaries?.mapM (return ·.getState (← getEnv))
+  if let some b := unfoldBoundaries? then
     srcType ← b.insertBoundaries srcType t.attrName
   srcType ← applyReplacementForall t cfg.dontTranslate srcType
   let reorder' := guessReorder srcType tgtDecl.type
@@ -942,7 +969,7 @@ partial def checkExistingType (t : TranslateData) (src tgt : Name) (cfg : Config
       If you need to give a hint to `{t.attrName}` to translate expressions involving `{src}`,\n\
       use `{t.attrName}_do_translate` instead"
   srcType ← reorderForall reorder srcType
-  if let some b := t.unfoldBoundaries? then
+  if let some b := unfoldBoundaries? then
     srcType ← b.unfoldInsertions srcType
   if reorder.any (·.contains 0) then
     srcDecl := srcDecl.updateLevelParams srcDecl.levelParams.swapFirstTwo

Mathlib/Tactic/Translate/UnfoldBoundary.lean
@@ -123,7 +123,7 @@ def mkAppWithCast (b : UnfoldBoundaries) (f a : Expr) : SimpM Expr :=
     return f.app (← mkCast b a d)
 
 /-- Modify `e` so that it has type `expectedType` if the constants in `b` cannot be unfolded. -/
-def UnfoldBoundaries.cast (b : UnfoldBoundaries) (e expectedType : Expr) (attr : Name) :
+public def UnfoldBoundaries.cast (b : UnfoldBoundaries) (e expectedType : Expr) (attr : Name) :
     MetaM Expr :=
   run b <|
   try
@@ -137,7 +137,7 @@ def UnfoldBoundaries.cast (b : UnfoldBoundaries) (e expectedType : Expr) (attr :
 Note: it may be that `e` contains some constant whose type is not well typed in this setting.
 We don't make an effort to replace such constants.
 It seems that this approximation works well enough. -/
-def UnfoldBoundaries.insertBoundaries (b : UnfoldBoundaries) (e : Expr) (attr : Name) :
+public def UnfoldBoundaries.insertBoundaries (b : UnfoldBoundaries) (e : Expr) (attr : Name) :
     MetaM Expr :=
   run b <| Meta.transform e (post := fun e ↦ e.withApp fun f args =>
     try
@@ -147,7 +147,7 @@ def UnfoldBoundaries.insertBoundaries (b : UnfoldBoundaries) (e : Expr) (attr : 
         well typed\n\n{ex.toMessageData}")
 
 /-- Unfold all of the auxiliary functions that were inserted as unfold boundaries. -/
-def UnfoldBoundaries.unfoldInsertions (e : Expr) (b : UnfoldBoundaries) : CoreM Expr :=
+public def UnfoldBoundaries.unfoldInsertions (e : Expr) (b : UnfoldBoundaries) : CoreM Expr :=
   -- This is the same as `Meta.deltaExpand`, but with an extra beta reduction.
   Core.transform e fun e => do
     if let some e ← delta? e b.insertionFuns.contains then
@@ -181,20 +181,5 @@ public def registerUnfoldBoundaryExt : IO UnfoldBoundaryExt := do
     addEntryFn := UnfoldBoundaries.insert
     addImportedFn as := as.foldl (Array.foldl (·.insert ·)) {}
   }
-
-@[inherit_doc UnfoldBoundaries.cast]
-public def UnfoldBoundaryExt.cast (b : UnfoldBoundaryExt) (e expectedType : Expr) (attr : Name) :
-    MetaM Expr := do
-  (b.getState (← getEnv)).cast e expectedType attr
-
-@[inherit_doc UnfoldBoundaries.insertBoundaries]
-public def UnfoldBoundaryExt.insertBoundaries (b : UnfoldBoundaryExt) (e : Expr) (attr : Name) :
-    MetaM Expr := do
-  (b.getState (← getEnv)).insertBoundaries e attr
-
-@[inherit_doc UnfoldBoundaries.unfoldInsertions]
-public def UnfoldBoundaryExt.unfoldInsertions (e : Expr) (b : UnfoldBoundaryExt) : CoreM Expr := do
-  (b.getState (← getEnv)).unfoldInsertions e
-
 
 end Mathlib.Tactic.UnfoldBoundary

MathlibTest/ToDual.lean
@@ -32,8 +32,9 @@ attribute [to_dual existing] Lattice.toSemilatticeInf
 
 -- we still cannot reorder arguments of arguments, so `SemilatticeInf.mk` is not translatable
 /--
-error: @[to_dual] failed. The translated value is not type correct. For help, see the docstring of `to_additive`, section `Troubleshooting`. Failed to add declaration
-instSemilatticeSupOfForallLeForallMax:
+error: @[to_dual] failed to add declaration `instSemilatticeSupOfForallLeForallMax`.
+  The translated value is not type correct.
+  For help, see the docstring of `to_additive`, section `Troubleshooting`.
 Application type mismatch: The argument
   le_inf
 has type